7a9556d14ede5ebda2634bb40673ae4dd8beae93
cHANGTEEZY <jagatgurung30@gmail.com>
2025-04-13 16:15:15 +0545

    file upload

--- client/src/components/Application/UploadApplication.tsx
+++ client/src/components/Application/UploadApplication.tsx
@@ -1,5 +1,9 @@
 const UploadApplication = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="border h-120 w-80 mt-6 rounded-3xl"></div>
+    </div>
+  );
 };
 
 export default UploadApplication;
--- client/src/pages/Application/FileUpload.tsx
+++ client/src/pages/Application/FileUpload.tsx
@@ -2,8 +2,8 @@ import UploadApplication from "@/components/Application/UploadApplication";
 
 const FileUpload = () => {
   return (
-    <section className="ml-5 mr-5 mt-5  border-t-2">
-      <div className="space-y-2">
+    <section className="ml-5 mr-5 mt-5  ">
+      <div className="space-y-2 pb-6 border-b">
         <h1 className="font-bold text-3xl">Upload Application Details</h1>
         <p className="text-sm text-slate-600 dark:text-slate-300">
           Upload your video file and other media in here
