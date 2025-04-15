eb6c9413297baa3251c1a5869340f0f599b5be3d
cHANGTEEZY <jagatgurung30@gmail.com>
2025-04-15 10:04:43 +0545

    add package

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,20 +1,26 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/Routes";
-import { ThemeProvider } from "./components/theme-provider";
-import { AuthProvider } from "./contexts/AuthContext";
-import { Toaster } from "./components/ui/sonner";
 import "./assets/styles/global.css";
+import "./index.css";
+
+import { StrictMode } from "react";
+import { router } from "./routes/Routes";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+
+import { AuthProvider } from "./contexts/AuthContext";
+import { DndContext } from "@dnd-kit/core";
+
+import { ThemeProvider } from "./components/theme-provider";
+import { Toaster } from "./components/ui/sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <AuthProvider>
-        <RouterProvider router={router} />
-        <Toaster position="top-right" />
-      </AuthProvider>
+      <DndContext>
+        <AuthProvider>
+          <RouterProvider router={router} />
+          <Toaster position="top-right" />
+        </AuthProvider>
+      </DndContext>
     </ThemeProvider>
-  </StrictMode>
+  </StrictMode>,
 );
